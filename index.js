71153756253814b0476494362b56d8e169d21bcf
aaron-might <aaronmighty7@gmail.com>
2021-09-02 03:25:53 -0400

    filter out unchecked types and write loop over length

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 // DOM elements
-const resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numberEl = document.getElementById('numbers')
-const symbolEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 const randomFunc = {
-    lower:getRandomLower,
-    upper:getRandomUpper,
-    number:getRandomNumber,
-    symbol:getRandomSymbol,
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
 }
 
-
+// generate event    
 generateEl.addEventListener('click', () => {
 const length = +lengthEl.value;
 const hasLower = lowercaseEl.checked;
 const hasUpper = uppercaseEl.checked;
-const hasNumber = numberEl.checked;
-const hasSymbol = symbolEl.checked;
-console.log(hasLower, hasUpper, hasNumber, hasSymbol);
-})
+const hasNumber = numbersEl.checked;
+const hasSymbol = symbolsEl.checked;
+
+resultEl.innerHTML = generatePassword(
+    hasLower, 
+    hasUpper, 
+    hasNumber, 
+    hasSymbol, 
+    length
+    );
+});
+
+// generate password function
+function generatePassword(lower, upper, number, symbol, length){
+    //     // add final pw to the pw var and return
+    //     // 1. init pw var
+    let generatedPassword ='';
+    const typesCount = lower + upper + number + symbol;
+    console.log('typesCount:', typesCount);
+    
+    //     // 2.filter out unchecked types
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+        item => Object.values(item)[0]
+        );
+        console.log('typesArr:', typesArr);
+    //     // 3.loop over length call generator function for each types
+
+if (typesCount === 0) {
+    return '';
+    for(let i=0; i<length; i += typesCount) {
+        typesArr.forEach(type =>{
+
+        });
+    }
+}
+}
 
 // Generator function -https://www.net-comber.com/charset.html
 function getRandomLower(){
@@ -41,4 +73,4 @@ function getRandomSymbol(){
     const symbols = '!@#$%^&*(){}[]=<>/,.';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log(getRandomSymbol());
+// console.log(getRandomSymbol());
