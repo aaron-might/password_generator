a9c877052cd942cbb019c44192272f4c28813a6b
aaron-might <aaronmighty7@gmail.com>
2021-09-02 04:28:52 -0400

    write generatedPassword function

--- index.js
+++ index.js
@@ -17,59 +17,65 @@ const randomFunc = {
 
 // generate event    
 generateEl.addEventListener('click', () => {
-const length = +lengthEl.value;
-const hasLower = lowercaseEl.checked;
-const hasUpper = uppercaseEl.checked;
-const hasNumber = numbersEl.checked;
-const hasSymbol = symbolsEl.checked;
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
 
-resultEl.innerHTML = generatePassword(
-    hasLower, 
-    hasUpper, 
-    hasNumber, 
-    hasSymbol, 
-    length
+    resultEl.innerHTML = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
     );
 });
 
 // generate password function
-function generatePassword(lower, upper, number, symbol, length){
+function generatePassword(lower, upper, number, symbol, length) {
     //     // add final pw to the pw var and return
     //     // 1. init pw var
-    let generatedPassword ='';
+    let generatedPassword = '';
     const typesCount = lower + upper + number + symbol;
-    console.log('typesCount:', typesCount);
-    
+    // console.log('typesCount:', typesCount);
+
     //     // 2.filter out unchecked types
     const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
         item => Object.values(item)[0]
-        );
-        console.log('typesArr:', typesArr);
+    );
+    // console.log('typesArr:', typesArr);
     //     // 3.loop over length call generator function for each types
 
-if (typesCount === 0) {
-    return '';
-    for(let i=0; i<length; i += typesCount) {
-        typesArr.forEach(type =>{
+    if (typesCount === 0) {
+        return '';
+    }
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            // console.log('funcName:', funcName);
 
+            generatedPassword += randomFunc[funcName]();
         });
     }
-}
+    const finalPassword = (generatedPassword.slice(0, length));
+    
+    return finalPassword;
 }
 
 // Generator function -https://www.net-comber.com/charset.html
-function getRandomLower(){
+function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-function getRandomUpper(){
+function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-function getRandomNumber(){
+function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 console.log(getRandomNumber());
 
-function getRandomSymbol(){
+function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
