249a10a5fca32711a9edcad5f403be0d7a9e2fe1
aaron-might <aaronmighty7@gmail.com>
2021-09-02 01:00:59 -0400

    write getrandomlower, getrandomupper, getrandomunber, getrandomsymbol

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-const resultEl = document.getElementById('result');
-
-const lengthEl = document.getElementById('length');
-
-const uppercaseEl = document.getElementById('uppercase');
-
-const lowercaseEl = document.getElementById('lowercase');
-
-const numbersEl = document.getElementById('numbers');
-
-const symbolsEl = document.getElementById('symbols');
-
-const generateEl = document.getElementById('generate');
-
-const clipboard = document.getElementById('clipboard');
-
-
-const randomFunc = {
-    lower:getRandomLower,
-    upper:getRandomUpper,
-    number:getRandomNumber,
-    symbol:getRandomSymbol
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
 
-clipboard.addEventListener('click', () => {
-    const textarea = document.createElement('textarea');
-
-    const password = resultEl.innerText;
-
-    if (!password) {return;}
-
-    textarea.value = password;
-    document.body.appendChild(textarea);
-
-    textarea.select();
-    document.execCommand('copy');
-
-    textarea.remove();
-    alert('Password copied to clipboard');
-});
-
-generate.addEventListener('click', () => {
-    const length = +lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
-    const hasnumber = numberEl.checked;
-    const hassymbol = symbolsEl.checked;
-})
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(getRandomSymbol());
